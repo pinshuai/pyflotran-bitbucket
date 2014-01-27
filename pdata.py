a605fb746788ca3cd82bd2e3f10d0e5fb85f32ab
Satish Karra <Satish Karra@localhost>
2014-01-27 15:26:58 -0700

    Added grid and mode related keywords

--- pdata.py
+++ pdata.py
@@ -12,8 +12,8 @@ WINDOWS = platform.system()=='Windows'
 if WINDOWS: copyStr = 'copy'; delStr = 'del'; slash = '\\'
 else: copyStr = 'cp'; delStr = 'rm'; slash = '/'
 
-cards = ['material_property','time']
-headers = ['material properties',
+cards = ['mode','grid','material_property','time']
+headers = ['mode','grid','material properties',
 		   'time stepping']
 headers = dict(zip(cards,headers))
 
@@ -74,7 +74,7 @@ class ptime(object):
 		self._tf = tf
 		self._dti = dti
 		self._dtf = dtf
-		
+	
 	def _get_tf(self): return self._tf
 	def _set_tf(self,value): self._tf = value
 	tf = property(_get_tf, _set_tf) #: (**)
@@ -84,6 +84,53 @@ class ptime(object):
 	def _get_dtf(self): return self._dtf
 	def _set_dtf(self,value): self._dtf = value
 	dtf = property(_get_dtf, _set_dtf) #: (**)
+class pgrid(object):
+	""" Class for grid property
+	
+	"""
+	
+	def __init__(self,type='structured',lower_bounds=[0.0,0.0,0.0],upper_bounds=[50.0,50.0,50.0],
+				 origin=[0.0,0.0,0.0],nxyz=[10,10,10],gravity=[0.0,0.0,-9.8068],filename=None):
+		self._type = type
+		self._lower_bounds = lower_bounds
+		self._upper_bounds = upper_bounds
+		self._origin = origin
+		self._nxyz = nxyz
+		self._gravity = gravity
+		self._filename = filename
+
+	def _get_type(self): return self._type
+	def _set_type(self,value): self._type = value
+	type = property(_get_type, _set_type) #: (**)
+	def _get_lower_bounds(self): return self._lower_bounds
+	def _set_lower_bounds(self,value): self._lower_bounds = value
+	lower_bounds = property(_get_lower_bounds, _set_lower_bounds) #: (**)
+	def _get_upper_bounds(self): return self._upper_bounds
+	def _set_upper_bounds(self,value): self._upper_bounds = value
+	upper_bounds = property(_get_upper_bounds, _set_upper_bounds) #: (**)
+	def _get_origin(self): return self._origin
+	def _set_origin(self,value): self._origin = value
+	origin = property(_get_origin, _set_origin) #: (**)
+	def _get_nxyz(self): return self._nxyz
+	def _set_nxyz(self,value): self._nxyz = value
+	nxyz = property(_get_nxyz, _set_nxyz) #: (**)
+	def _get_gravity(self): return self._gravity
+	def _set_gravity(self,value): self._gravity = value
+	gravity = property(_get_gravity, _set_gravity) #: (**)
+	def _get_filename(self): return self._filename
+	def _set_filename(self,value): self._filename = value
+	filename = property(_get_filename, _set_filename) #: (**)
+class pmode(object):
+	""" Class for mode
+
+	"""
+
+	def __init__(self,name='richards'):
+		self._name = name
+
+	def _get_name(self): return self._name
+	def _set_name(self,value): self._name = value
+	name = property(_get_name, _set_name)
 class pdata(object):
 	"""Class for pflotran data file
 	
@@ -94,6 +141,8 @@ class pdata(object):
 		self._filename = filename 		
 		self._proplist = []
 		self._time = ptime()
+		self._mode = ''
+		self._grid = ''
 		
 		if filename: self.read(filename) 		# read in file if requested upon initialisation
 	def __repr__(self): return self.filename 	# print to screen when called
@@ -101,7 +150,9 @@ class pdata(object):
 		if not os.path.isfile(filename): print filename + ' not found...'
 		self._filename = filename 				# assign filename attribute
 		read_fn = dict(zip(cards, 				# associate each card name with a read function, defined further below
-				[self._read_prop,
+				[self._read_mode,
+         self._read_grid,
+				 self._read_prop,
 				 self._read_time]
 				 ))
 		with open(self._filename,'r') as infile:
@@ -113,7 +164,7 @@ class pdata(object):
 				if len(line.strip())==0: continue
 				card = line.split()[0].lower() 		# make card lower case
 				if card in cards: 					# check if a valid cardname
-					if card == 'material_property':
+					if card in ['material_property','mode','grid']:
 						read_fn[card](infile,line)
 					else:
 						read_fn[card](infile)
@@ -125,9 +176,97 @@ class pdata(object):
 		"""
 		if filename: self._filename = filename
 		outfile = open(self.filename,'w')
+		if self.mode: self._write_mode(outfile)
+		if self.grid: self._write_grid(outfile)
 		if self.time: self._write_time(outfile)
 		if self.proplist: self._write_prop(outfile)
 		outfile.close()
+	def _read_mode(self,infile,line):
+		mode_name = line.split()[-1]
+		new_mode = pmode(mode_name)
+		self._mode = new_mode
+	def _write_mode(self,outfile):
+		self._header(outfile,headers['mode'])
+		outfile.write('MODE\t')
+		outfile.write(self.mode.name+'\n\n')
+	def _read_grid(self,infile,line):
+		g = pgrid()				# assign defaults before reading in values
+		ng_type = g.type
+		ng_lower_bounds = g.lower_bounds
+		ng_upper_bounds = g.upper_bounds
+		ng_origin = g.origin
+		ng_nxyz = g.nxyz
+		ng_gravity = g.gravity
+		ng_filename = g.filename
+		
+		keepReading = True
+		while keepReading:
+			line = infile.readline() 			# get next line
+			key = line.strip().split()[0].lower() 		# take first keyword
+			if key == 'type':
+				ng_type = line.split()[-1]
+			elif key == 'bounds':
+				keepReading2 = True
+				while keepReading2:
+					line1 = infile.readline()	
+					ng_lower_bounds[0] = floatD(line1.split()[0])
+					ng_lower_bounds[1] = floatD(line1.split()[1])
+					ng_lower_bounds[2] = floatD(line1.split()[2])
+					line2 = infile.readline()
+					ng_upper_bounds[0] = floatD(line2.split()[0])
+					ng_upper_bounds[1] = floatD(line2.split()[1])
+					ng_upper_bounds[2] = floatD(line2.split()[2])
+					line3 = infile.readline()
+					if line3.strip().split()[0].lower() in ['/','end']: keepReading2 = False
+			elif key == 'origin':
+				ng_origin[0] = floatD(line.strip().split()[1])
+				ng_origin[1] = floatD(line.strip().split()[2])
+				ng_origin[2] = floatD(line.strip().split()[3])
+			elif key == 'nxyz':
+				ng_nxyz[0] = floatD(line.split()[1])
+				ng_nxyz[1] = floatD(line.split()[2])
+				ng_nxyz[2] = floatD(line.split()[3])
+			elif key == 'gravity':
+				ng_gravity[0] = floatD(line.split()[1])
+				ng_gravity[1] = floatD(line.split()[2])
+				ng_gravity[2] = floatD(line.split()[3])
+			elif key == 'filename':
+				if ng_type != 'unstructured': print 'Error - filename not need with structure grid'
+				ng_filename = line.split()[-1]
+			elif key in ['/','end']: keepReading = False
+		new_grid = pgrid(ng_type,ng_lower_bounds,ng_upper_bounds,ng_origin,ng_nxyz,
+						 ng_gravity,ng_filename) 		# create an empty grid
+		self._grid = new_grid
+	def _write_grid(self,outfile):
+		self._header(outfile,headers['grid'])
+		grid = self.grid
+		outfile.write('GRID\n')
+		outfile.write('\tTYPE\t' + grid.type + '\n')
+		outfile.write('\tBOUNDS\n')
+		outfile.write('\t\t')
+		for i in range(3):
+			outfile.write(str(grid.lower_bounds[i]) + ' ')
+		outfile.write('\n')
+		outfile.write('\t\t')
+		for i in range(3):
+			outfile.write(str(grid.upper_bounds[i]) + ' ')
+		outfile.write('\n')
+		outfile.write('\tEND\n')
+		outfile.write('\tORIGIN' + ' ')
+		for i in range(3):
+			outfile.write(str(grid.origin[i]) + ' ')
+		outfile.write('\n')
+		outfile.write('\tNXYZ' + ' ')
+		for i in range(3):
+			outfile.write(str(grid.nxyz[i]) + ' ')
+		outfile.write('\n')
+		outfile.write('\tGRAVITY' + ' ')
+		for i in range(3):
+			outfile.write(str(grid.gravity[i]) + ' ')
+		outfile.write('\n')
+		if grid.type == 'unstructured':
+			outfile.write('\tFILENAME' + grid.filename + '\n')
+		outfile.write('END\n\n')
 	def _read_prop(self,infile,line):
 		np_name = line.split()[-1] 		# property name
 		np_id=None
@@ -253,6 +392,8 @@ class pdata(object):
 		for i in range(pad): ws+='='
 		ws+='\n'
 		outfile.write(ws)
+	def _get_grid(self): return self._grid
+	grid = property(_get_grid) #: (**)
 	def _get_time(self): return self._time
 	time = property(_get_time) #: (**)
 	def _get_proplist(self): return self._proplist
@@ -262,6 +403,6 @@ class pdata(object):
 	def _get_filename(self): return self._filename
 	def _set_filename(self,value): self._filename = value
 	filename = property(_get_filename, _set_filename) #: (**)	
-		
-		
+	def _get_mode(self): return self._mode
+	mode = property(_get_mode)
 		

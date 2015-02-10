35140894f67de94538605327dbfb8e0a8c7d3071
Satish Karra <Satish Karra@localhost>
2015-02-09 17:05:24 -0700

    Added tracer 1D problems to test suite.

---
+++ tests/tracer_1D/test_tracer1D_read.py
@@ -0,0 +1,20 @@
+import filecmp
+import unittest
+import os
+
+dir = os.path.dirname(os.path.realpath(__file__))
+
+def compare_tracer1D():
+    """Return True if pyflotran reads tracer_1D.in correctly."""
+    os.system('python ' + dir + '/tracer_1D_read.py >& /dev/null')
+    return  filecmp.cmp(dir + '/tracer_1D_SC_2.in', dir + '/tracer_1D.gold')
+
+class tracer1D_read(unittest.TestCase):
+    """Test for reading tracer1D."""
+
+    def test_tracer1D_read(self):
+        """Test for reading tracer 1D"""
+        self.assertTrue(compare_tracer1D())
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/tracer_1D_pyflotran/test_tracer1D_pyflotran.py
@@ -0,0 +1,20 @@
+import filecmp
+import unittest
+import os
+
+dir = os.path.dirname(os.path.realpath(__file__))
+
+def compare_tracer1D():
+    """Return True if pyflotran runs tracer_1D_pyflotran.py in correctly."""
+    os.system('python ' + dir + '/tracer_1D_pyflotran.py >& /dev/null ')
+    return  filecmp.cmp(dir + '/tracer_1D.in', dir + '/tracer_1D.gold')
+
+class tracer1D_read(unittest.TestCase):
+    """Test for reading tracer1D."""
+
+    def test_tracer1D_read(self):
+        """Test for writing to PFLOTRAN input from PyFLOTRAN input for tracer 1D"""
+        self.assertTrue(compare_tracer1D())
+
+if __name__ == '__main__':
+    unittest.main()

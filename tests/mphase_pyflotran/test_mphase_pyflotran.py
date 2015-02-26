a351fae405bd242944509ce81243fda096e03425
Satish Karra <Satish Karra@localhost>
2015-02-25 17:34:59 -0700

    Updated mphase gold file to reflect the recent changes.

--- tests/mphase_pyflotran/test_mphase_pyflotran.py
+++ tests/mphase_pyflotran/test_mphase_pyflotran.py
@@ -9,13 +9,16 @@ def compare_mphase():
     os.system('python ' + dir + '/mphase.py >& /dev/null')
     return  filecmp.cmp(dir + '/mphase.in', dir + '/mphase.gold')
 
-
 class mphase_read(unittest.TestCase):
     """Test for writing to PFLOTRAN input from PyFLOTRAN input for mphase."""
 
     def test_mphase_read(self):
         """Test for writing to PFLOTRAN input from PyFLOTRAN input for mphase."""
         self.assertTrue(compare_mphase())
+	os.system('rm -f ' + dir + '/mphase.in')
+	os.system('rm -f ' + dir + '/mphase.out')
+	os.system('rm -f ' + dir + '/mphase*.tec')
+
 
 if __name__ == '__main__':
     unittest.main()

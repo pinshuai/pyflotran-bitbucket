4a7eff08827aba98b7d16f3972e1e9a098439ff1
Satish Karra <Satish Karra@localhost>
2017-11-07 16:25:28 -0700

    added uniaxial loading test.

---
+++ tests/uniaxial_loading/uniaxial_loading_read.py
@@ -0,0 +1,31 @@
+import sys
+import os
+from pdata import *
+
+
+try:
+    pflotran_dir = os.environ['PFLOTRAN_DIR']
+except KeyError:
+    print('PFLOTRAN_DIR must point to PFLOTRAN installation directory \
+           and be defined in system environment variables.')
+    sys.exit(1)
+
+try:
+    pyflotran_dir = os.environ['PYFLOTRAN_DIR']
+except KeyError:
+    print('PYFLOTRAN_DIR must point to PYFLOTRAN installation directory \
+           and be defined in system environment variables.')
+    sys.exit(1)
+
+test_dir = '/tests/uniaxial_loading/'
+
+###############################################################
+
+# Read Test Data
+dat = pdata(pyflotran_dir + test_dir + 'uniaxial_loading.in')
+
+# Test write
+dat.write(pyflotran_dir + test_dir + 'uniaxial_loading2.in')
+
+# Writing to a different file and executing that file
+# dat.run(input='vsat_flow2.in',exe=pflotran_dir + '/src/pflotran/pflotran')
